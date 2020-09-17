6b24439eb6a90f8f8ea9f9e8382412063e516426
Maxime Peloutier <maxime.peloutier@etu.univ-lyon1.fr>
2020-09-17 18:37:07 +0200

    Ajoute un compteur de blocs "cassés" accessible par une fonction globale get_nb_blocs()

--- mods/blocs_fragiles/init.lua
+++ mods/blocs_fragiles/init.lua
@@ -12,16 +12,29 @@ minetest.register_node(minetest.get_current_modname()..":bloc_fragile_normal_dis
   groups = {oddly_breakable_by_hand=1,},
 })
 
+-- Fonction qui fait réapparaître un bloc du nom donné à proximité de la position donnée
+local function reapparition_bloc(pos, node_name)
+  -- Réappariton d'un bloc à une position aléatoire à proximité du bloc disparu
+  local pos = {x = pos.x + math.random(0,6) - 3, y = pos.y, z = pos.z + math.random(0,6) - 3}
+  minetest.set_node(pos, {name = node_name})
+end
+
+local nb_blocs = 1
 -- Fonction qui fait disparaître un bloc en le remplaçant par un bloc d'air après quelques secondes
 local function disparition_bloc(player, pos, node, desc)
-    minetest.set_node(pos, {name = "blocs_fragiles:bloc_fragile_normal_disp"})
-    minetest.after(3, function()
-        minetest.set_node(pos, {name = "air"})
-        -- Réappariton d'un bloc à une position aléatoire à proximité du bloc disparu
-        local pos = {x = pos.x + math.random(0,6) - 3, y = pos.y, z = pos.z + math.random(0,6) - 3}
-        minetest.set_node(pos, {name = "blocs_fragiles:bloc_fragile_normal"})
-    end)
+  minetest.set_node(pos, {name = "blocs_fragiles:bloc_fragile_normal_disp"})
+  minetest.after(3, function()
+    minetest.set_node(pos, {name = "air"})
+    reapparition_bloc(pos, "blocs_fragiles:bloc_fragile_normal")
+  end)
+  nb_blocs = nb_blocs + 1
+  -- minetest.chat_send_all(nb_blocs)
 end
 
 -- Ajouter un effet quand un joueur marche sur le bloc
 poschangelib.add_player_walk_listener("blocs_fragiles:ecouteur_bfn", disparition_bloc, {'blocs_fragiles:bloc_fragile_normal'})
+
+-- Retourne le nombre de blocs sur lesquels le joueur est passé
+function get_nb_blocs()
+  return(nb_blocs)
+end
