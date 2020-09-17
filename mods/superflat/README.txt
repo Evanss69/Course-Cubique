a9e3dc912596aec0c9f8ca1e523245b00b33072c
Maxime Peloutier <maxime.peloutier@etu.univ-lyon1.fr>
2020-09-17 19:11:53 +0200

    Ajoute le mod superflat qui sert à créer des mondes plats

---
+++ mods/superflat/README.txt
@@ -0,0 +1,72 @@
+(Yet Another) Superflat Map Generator [superflat]
+=========
+
+# License
+MIT License (code) and CC BY-SA 3.0 (media) (see LICENSE file)
+
+# Feature
+- Costumize world generation layer by layer
+- Decoration (optional)
+- If you fall, you will be returned to surface
+
+# How to Modify
+1. Open parameter.lua file.
+2. Change sflat.Y_ORIGIN to adjust first layer's pos (default: 1).
+3. Change sflat.BLOCKS to adjust the composition of the world (code: layers from bottom to top;biome,decoration) (examples available).
+
+Note: If the specified node does not exist, the air node will be used.
+
+## Modify existing world
+1. Open superflat.txt file in your world's folder.
+2. Do like editing parameter.lua file.
+
+## List of biomes
+- Frozen River
+- River
+- Ice Plains
+- Ice Plains Spikes
+- Flower Plains
+- Plains
+- Forest
+- Jungle
+- Desert
+
+## Presets
+Preset
+	Code
+
+Example
+	"node:name_bottom=amount,node:name_top=amount"
+Example (+decoration)
+	"node:name_bottom=amount,node:name_top=amount;Biome,decoration"
+Example (one layer thick)
+	"node:a,node:b=2,node:c=3"
+
+Default
+	"superflat:bedrock,default:dirt=2,default:dirt_with_grass"
+Forest
+	"superflat:bedrock,default:dirt=2,default:dirt_with_grass;Forest,decoration"
+Shallow Underground
+	"superflat:bedrock,default:stone=230,default:dirt=5,default:dirt_with_grass"
+Deep Underground
+	"superflat:bedrock,default:stone=920,default:dirt=10,default:dirt_with_grass"
+Very Deep Underground
+	"superflat:bedrock,default:stone=3680,default:dirt=15,default:dirt_with_grass"
+Bottomless Pit
+	"default:cobble=2,default:dirt=3,default:dirt_with_grass"
+Shallow Sea
+	"superflat:bedrock,default:dirt=3,default:water_source=5"
+Sea
+	"superflat:bedrock,default:dirt=3,default:water_source=10"
+Deep Sea
+	"superflat:bedrock,default:dirt=3,default:water_source=20"
+Water World
+	"superflat:bedrock,default:dirt=3,default:water_source=60"
+Beach
+	"superflat:bedrock,default:sand=3,default:water_source"
+Desert Superflat
+	"superflat:bedrock,default:desert_sand=3"
+Farmer's Dream
+	"superflat:bedrock,default:water_source,farming:soil_wet"
+You Cannot Escape
+	"default:gravel,default:sand,default:gravel"
---
+++ mods/superflat/init.lua
@@ -0,0 +1,120 @@
+-- (Yet Another) Superflat Map Generator
+-- Modify parameter in parameter.lua!
+
+--------------------
+-- Internal stuff --
+--------------------
+
+if sflat == nil then sflat = {} end
+sflat.options = {
+	biome = "",
+	decoration = false
+}
+
+local c_air = minetest.get_content_id("air")
+function sflat.get_content_id(name)
+	local id = c_air
+	if minetest.registered_nodes[name] then
+		id = minetest.get_content_id(name)
+	end
+	return id
+end
+
+dofile(minetest.get_modpath("superflat") .. "/parsetext.lua")
+dofile(minetest.get_modpath("superflat") .. "/parameter.lua")
+dofile(minetest.get_modpath("superflat") .. "/decoration.lua")
+
+minetest.register_on_mapgen_init(function(mgparams)
+	minetest.set_mapgen_setting("mg_name", "singlenode", true)
+end)
+
+-- Superflat's bedrock
+minetest.register_node("superflat:bedrock", {
+	description = "Superflat's Bedrock",
+	tiles = {"superflat_bedrock.png"},
+	groups = {unbreakable = 1, not_in_creative_inventory = 1},
+	sounds = default.node_sound_stone_defaults()
+})
+
+-- Read and check if superflat.txt file exists
+-- If there is no text at superflat.txt, it will use default setting.
+if file_exists(minetest.get_worldpath() .. DIR_DELIM .. "superflat.txt") == true then
+	dofile(minetest.get_worldpath() .. DIR_DELIM .. "superflat.txt")
+else
+	local list = io.open(minetest.get_worldpath() .. DIR_DELIM .. "superflat.txt", "w")
+	list:write(
+		"sflat.Y_ORIGIN = " .. sflat.Y_ORIGIN .. "\n" ..
+		"sflat.BLOCKS = \"" .. sflat.BLOCKS .. "\""
+	)
+	list:close()
+end
+
+local LAYERS = nil
+-- Wait until all nodes are loaded
+minetest.after(1, function()
+	if LAYERS == nil then
+		LAYERS = sflat.parsetext(sflat.BLOCKS)
+	end
+end)
+
+minetest.register_on_generated(function(minp, maxp, seed)
+	if minp.y >= LAYERS[#LAYERS][3] then
+		return
+	end
+	local t1 = os.clock()
+	local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+	local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
+	local data = vm:get_data()
+	local pr = PseudoRandom(seed + 1)
+	
+	-- Generate layers
+	local li = 1
+	for y = minp.y, maxp.y do
+		if li > #LAYERS then
+			break
+		end
+		if y >= LAYERS[li][3] then
+			li = li + 1
+		end
+		if (y >= sflat.Y_ORIGIN and y < LAYERS[#LAYERS][3]) then
+			local block = LAYERS[li][2]
+			for z = minp.z, maxp.z do
+			for x = minp.x, maxp.x do
+				local vi = area:index(x, y, z)
+				data[vi] = block
+			end
+			end
+		else
+			-- air
+		end
+	end
+	
+	-- Decorate terrain if enabled
+	if sflat.options.decoration == true then
+		sflat.decoration.generate(minp, maxp, LAYERS, data, area, seed, pr)
+	end
+	
+	vm:set_data(data)
+	vm:set_lighting({day = 0, night = 0})
+	vm:update_liquids()
+	vm:calc_lighting()
+	vm:write_to_map(data)
+end)
+
+sflat.bedrock_timer = 0
+minetest.register_globalstep(function(dtime)
+	sflat.bedrock_timer = sflat.bedrock_timer - dtime
+	if sflat.bedrock_timer > 0 then return end
+	sflat.bedrock_timer = 1
+	for k,player in ipairs(minetest.get_connected_players()) do
+		local pos = player:get_pos()
+		if pos.y < sflat.Y_ORIGIN - 1 then
+			-- Prepare space for falling players
+			minetest.set_node({x = pos.x, y = LAYERS[#LAYERS][3] + 1, z = pos.z}, {name = "air"})
+			minetest.set_node({x = pos.x, y = LAYERS[#LAYERS][3], z = pos.z}, {name = "air"})
+			minetest.set_node({x = pos.x, y = LAYERS[#LAYERS][3] - 1, z = pos.z}, {name = LAYERS[#LAYERS][1]})
+			-- Teleport them back to surface
+			player:set_pos({x = pos.x, y = LAYERS[#LAYERS][3] - 0.5, z = pos.z})
+		end
+	end
+end)
