7b462e351730559e3758506093d0f36538fbfc82
evan saint-sorny <you@example.com>
2020-09-22 17:20:26 +0200

    ajout timer & score + block delimitation

---
+++ mods/blocs_arene/init.lua
@@ -0,0 +1,6 @@
+minetest.register_node(minetest.get_current_modname()..":bloc_delimitation_sb",
+{
+  description = "A utiliser pour constrtuire la délimitation de la zone de Square Block",
+  tiles = {"^[colorize:#B8860B"},
+  groups = {oddly_breakable_by_hand=1,},
+})
--- mods/blocs_timer/init.lua
+++ mods/blocs_timer/init.lua
@@ -1,20 +1,38 @@
--- Propriétés du timer
-local timerjeu = Timer(function(elapsed)
-        print("fn3: ", elapsed)
-    end, {
-    interval = 300, -- Durée maximale du timer
-    repeats = false, -- Le timer prend fin quand il atteint la valeur interval
+
+--Propriétés du timer
+local timerjeu2 = Timer(function(elapsed)
+  print("fn3: ", elapsed)
+end, {
+interval = 300, -- Durée maximale du timer
+repeats = false, -- Le timer prend fin quand il atteint la valeur interval
 })
+
+
+score=1500
+
+function diminution_score()
+  minetest.after(10,function()
+		if(timerjeu2:is_active()) then
+		score=score-5
+    diminution_score()
+		end
+  end	)
+	
+end
+	
+
+
 
 minetest.register_node(minetest.get_current_modname()..":bloc_depart",
 {
   description = "Déclenche le timer quand on marche dessus!",
-  tiles = {"^[colorize:#ffeb3b"},
+  tiles = {"blocs_timer_depart.png"},
   groups = {oddly_breakable_by_hand=1,},
 })
 
 local function depart_timer(player, pos, node, desc)
-    timerjeu:start()
+    timerjeu2:start()
+    diminution_score()
 end
 
 minetest.register_node(minetest.get_current_modname()..":bloc_intervalle",
@@ -25,20 +43,23 @@ minetest.register_node(minetest.get_current_modname()..":bloc_intervalle",
 })
 
 local function temps_ecoule(player, pos, node, desc)
-    minetest.chat_send_all(timerjeu:get_elapsed())
+    minetest.chat_send_all(timerjeu2:get_elapsed())
 end
 
 minetest.register_node(minetest.get_current_modname()..":bloc_fin",
 {
   description = "Arrête le timer quand on marche dessus!",
-  tiles = {"^[colorize:#ffeb3b"},
+  tiles = {"blocs_timer_arrivee.png"},
   groups = {oddly_breakable_by_hand=1,},
 })
 
 local function fin_timer(player, pos, node, desc)
     minetest.chat_send_all('Temps écoulé: ')
-    minetest.chat_send_all(timerjeu:get_elapsed())
-    timerjeu:stop()
+    minetest.chat_send_all(timerjeu2:get_elapsed())
+    timerjeu2:stop()
+    timerjeu2:expire()
+    minetest.chat_send_all("Ton score est :")
+    minetest.chat_send_all(score)
 end
 
 -- Ajout des écouteurs de chaque bloc
