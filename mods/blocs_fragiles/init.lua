4c2c63651608d7cf17264e2df1fa57d4bb5b4ef9
Maxime Peloutier <maxime.peloutier@etu.univ-lyon1.fr>
2020-10-01 11:13:10 +0200

    Correction du commit précédent

--- mods/blocs_fragiles/init.lua
+++ mods/blocs_fragiles/init.lua
@@ -1,15 +1,49 @@
 minetest.register_node(minetest.get_current_modname()..":bloc_fragile_normal",
 {
   description = "Disparait après quelques secondes quand on marche dessus!",
-  tiles = {"^[colorize:#e58703"},
+  tiles = {"^[colorize:#E58703"},
   groups = {oddly_breakable_by_hand=1,},
 })
 
 minetest.register_node(minetest.get_current_modname()..":bloc_fragile_normal_disp",
 {
+  drawtype = glasslike,
+  use_texture_alpha = true,
   description = "Ce bloc est en train de disparaitre!",
-  tiles = {"^[colorize:#5f3901"},
+  tiles = {"^[colorize:#E58703B3"},
   groups = {oddly_breakable_by_hand=1,},
+})
+
+minetest.register_node(minetest.get_current_modname()..":bloc_fragile_bond",
+{
+  description = "Propulse le joueur en l'air quand on marche dessus!",
+  tiles = {"^[colorize:#05D210"},
+  groups = {oddly_breakable_by_hand=1,},
+})
+
+minetest.register_node(minetest.get_current_modname()..":bloc_fragile_bond_disp",
+{
+  drawtype = glasslike,
+  use_texture_alpha = true,
+  description = "Ce bloc est en train de disparaitre!",
+  tiles = {"^[colorize:#05D2104D"},
+  groups = {oddly_breakable_by_hand=1,},
+})
+
+minetest.register_node(minetest.get_current_modname()..":bloc_fragile_gliss",
+{
+  description = "Un bloc glissant!",
+  tiles = {"^[colorize:#07F4F8"},
+  groups = {oddly_breakable_by_hand=1, slippery = 3},
+})
+
+minetest.register_node(minetest.get_current_modname()..":bloc_fragile_gliss_disp",
+{
+  drawtype = glasslike,
+  use_texture_alpha = true,
+  description = "Ce bloc est en train de disparaitre!",
+  tiles = {"^[colorize:#07F4F84D"},
+  groups = {oddly_breakable_by_hand=1, slippery = 3},
 })
 
 -- Fonction qui fait réapparaître un bloc du nom donné à proximité de la position donnée
@@ -29,16 +63,25 @@ end
 local nb_blocs = 0
 -- Fonction qui fait disparaître un bloc en le remplaçant par un bloc d'air après quelques secondes
 local function disparition_bloc(player, pos, node, desc)
-  minetest.set_node(pos, {name = "blocs_fragiles:bloc_fragile_normal_disp"})
+  -- Transforme le bloc en sa version disparaîssante
+  minetest.set_node(pos, {name = node.name.."_disp"})
   minetest.after(3, function()
     minetest.set_node(pos, {name = "air"})
-    reapparition_bloc(pos, "blocs_fragiles:bloc_fragile_normal")
+    reapparition_bloc(pos, node.name)
   end)
   nb_blocs = nb_blocs + 1
 end
 
+-- Fonction qui propulse le joueur en l'air puis fait disparaître le bloc
+local function bond(player, pos, node, desc)
+  player:add_player_velocity({x=0, y=20, z=0})
+  disparition_bloc(player, pos, node, desc)
+end
+
 -- Ajouter un effet quand un joueur marche sur le bloc
 poschangelib.add_player_walk_listener("blocs_fragiles:ecouteur_bfn", disparition_bloc, {'blocs_fragiles:bloc_fragile_normal'})
+poschangelib.add_player_walk_listener("blocs_fragiles:ecouteur_bfb", bond, {'blocs_fragiles:bloc_fragile_bond'})
+poschangelib.add_player_walk_listener("blocs_fragiles:ecouteur_bfg", disparition_bloc, {'blocs_fragiles:bloc_fragile_gliss'})
 
 -- Setter et Getter nombre de blocs sur lesquels le joueur est passé
 function set_nb_blocs(nb)
@@ -48,3 +91,4 @@ end
 function get_nb_blocs()
   return(nb_blocs)
 end
+
